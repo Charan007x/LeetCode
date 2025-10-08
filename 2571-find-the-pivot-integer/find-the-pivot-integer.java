59a37ed6a1ab2b041ea1c62e4c34f2c9e2be25e6
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2025-10-08 19:37:29 +0530

    Time: 1 ms (66.35%) | Memory: 40.5 MB (91.35%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int max=(n*(n+1))/2;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+            if(sum==max-sum+i) return i;
+        }
+        return -1;
+    }
+}
