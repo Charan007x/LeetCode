7e31d868961d1ea41630e8a3fa5508492489b54f
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2026-02-08 16:14:04 +0530

    Time: 25 ms (5.13%) | Memory: 50.6 MB (99.10%) - LeetSync

---
+++ 89-gray-code/gray-code.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> list = new ArrayList<>();
+        int sw[]=new int[1];
+        sw[0]=0;
+        fun(n,0,"",list,sw);
+        return list;
+    }
+    public static void fun(int n,int idx,String s,List<Integer> list,int sw[]){
+        if(idx==n){
+            list.add(Integer.parseInt(s,2));
+            sw[0]++;
+            return;
+        }
+        if(sw[0]%2==0){
+        fun(n,idx+1,s+"0",list,sw);
+        sw[0]++;
+        fun(n,idx+1,s+"1",list,sw);
+        sw[0]++;
+        }else{
+            sw[0]++;
+            fun(n,idx+1,s+"1",list,sw);
+            sw[0]++;
+            fun(n,idx+1,s+"0",list,sw);
+        }
+    }
+}
