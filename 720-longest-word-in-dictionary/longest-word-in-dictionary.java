ceb677c67f529d5292b57cf50082ec9eba1795bd
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2026-03-10 15:42:30 +0530

    Time: 45 ms (7.59%) | Memory: 46.4 MB (87.57%) - LeetSync

---
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String longestWord(String[] a) {
+        HashSet<String> set = new HashSet<>();
+        for(int i=0;i<a.length;i++) set.add(a[i]);
+        String ans="";
+        Arrays.sort(a);
+        for(int i=a.length-1;i>=0;i--){
+            String s=a[i];
+            boolean flag=true;
+            for(int j=s.length();j>0;j--){
+                if(!set.contains(s.substring(0,j))) flag=false;
+            }
+            if(flag){
+                if(s.length()==ans.length()){
+                    if(s.compareTo(ans)<0){
+                        ans=s;
+                    }
+                }else if(s.length()>ans.length()){
+                    ans=s;
+                }
+            }
+        }
+        return ans;
+    }
+}
