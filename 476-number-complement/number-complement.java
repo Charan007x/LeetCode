7d7cc10c6369b69502cd99570d8fc7921e645c1d
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2026-03-11 09:24:07 +0530

    Time: 1 ms (22.79%) | Memory: 42 MB (94.73%) - LeetSync

---
+++ 476-number-complement/number-complement.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findComplement(int n) {
+        String s=Integer.toString(n,2);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++) sb.append((s.charAt(i)=='1')?'0':'1');
+        s=sb.toString();
+        return Integer.parseInt(s,2);
+    }
+}
