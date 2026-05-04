b53364ac58a6356cc783f3a677b4d59114652aec
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2026-05-04 20:06:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (98.84%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotate(int[][] a) {
+       int n = a.length;
+       int x = 1;
+       for(int i=0;i<n;i++){ // transpose 
+        for(int j=x;j<n;j++){
+            int t=a[i][j];
+            a[i][j]=a[j][i];
+            a[j][i]=t;
+        }
+        x++;
+       }
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n/2;j++){
+            int t=a[i][j];
+            a[i][j]=a[i][n-1-j];
+            a[i][n-1-j]=t;
+        }
+    }
+    }
+}
