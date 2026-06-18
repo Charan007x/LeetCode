a1597665db1d667868dc894882c58a9c56c5df90
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2026-06-18 07:29:14 +0530

    Time: 398 ms (5.04%) | Memory: 286.5 MB (5.04%) - LeetSync

---
+++ 474-ones-and-zeroes/ones-and-zeroes.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int len=strs.length;
+        int b[]=new int[len];
+        for(int i=0;i<len;i++){
+            String s=strs[i];
+            int count=0;
+            for(int j=0;j<s.length();j++){
+                if(s.charAt(j)=='1') count++;
+            }
+            b[i]=count;
+        }
+        int dp[][][]=new int[m+1+200][n+1+200][len];
+        for(int i[][]:dp){
+            for(int j[]:i){
+                Arrays.fill(j,-1);
+            }
+        }
+        int ans=fun(strs,b,m,n,len-1,dp);
+        return (ans<=0?0:ans);
+    }
+    public static int fun(String []s,int b[],int m,int n,int idx,int dp[][][]){
+        if(idx<0){
+            if(m>=0&&n>=0)return 0;
+            return -50000;
+        }
+        if(m<0||n<0) return -50000;
+        if(dp[m+200][n+200][idx]!=-1) return dp[m+200][n+200][idx];
+        int left=1+fun(s,b,m-(s[idx].length()-b[idx]),n-b[idx],idx-1,dp);
+        int right=fun(s,b,m,n,idx-1,dp);
+        return dp[m+200][n+200][idx]=Math.max(left,right);
+    }
+}
