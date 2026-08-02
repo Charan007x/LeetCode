68504c8661233e1836102fde91a9ecf65767684d
Sai Charan Gottipalli <saicharangottipalli@gmail.com>
2026-08-02 13:43:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (62.03%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,29 +1,5 @@
 class Solution {
-    public boolean stoneGame(int[] a) {
-        int n = a.length;
-        int l=0,h=n-1;
-        int alice=0,bob=0;
-        for(int i=0;i<n;i++){
-            if(i%2==0){ // alice
-               if(a[l]>=a[h]){
-                 alice+=a[l];
-                 l++;
-            }else{
-                alice+=a[h];
-                h--;
-            }
-            }
-            else{ // bob
-            if(a[l]>=a[h]){
-                bob+=a[h];
-                h--;
-            }else{
-                bob+=a[l];
-                l++;
-            }
-            }
-        }
-            if(alice>bob) return true;
-            else return false;
+    public boolean stoneGame(int[] piles) {
+        return true;
     }
 }
